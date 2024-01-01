49112dc6cf8b141c7e7b352b6320fff934f4739c
Krish90s <krishnanvenkat4@gmail.com>
2024-01-01 21:46:37 +0530

    login and logout added

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,7 +5,14 @@ import React from "react";
 import { SiInternetcomputer } from "react-icons/si";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const Navbar = () => {
   const currentPath = usePathname();
@@ -18,34 +25,58 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="flex space-x-5 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <SiInternetcomputer />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.link}>
-            <Link
-              className={classNames({
-                "text-zinc-900": link.link === currentPath,
-                "text-zinc-500": link.link !== currentPath,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-              href={link.link}
-            >
-              {link.label}
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <SiInternetcomputer />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="api/auth/signout">Sign Out</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="api/auth/signin">Login</Link>
-        )}
-      </Box>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.link}>
+                  <Link
+                    className={classNames({
+                      "text-zinc-900": link.link === currentPath,
+                      "text-zinc-500": link.link !== currentPath,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                    href={link.link}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback="?"
+                    size="2"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="api/auth/signout">Sign Out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
