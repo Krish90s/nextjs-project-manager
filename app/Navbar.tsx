0ff9c1dbf90a28b28543517360a9755655b5009e
Krish90s <krishnanvenkat4@gmail.com>
2024-01-02 14:25:23 +0530

    Navbar updated

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -15,15 +15,6 @@ import {
 } from "@radix-ui/themes";
 
 const Navbar = () => {
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-
-  const links = [
-    { label: "Dashboard", link: "/" },
-    { label: "Projects", link: "/projects/list" },
-    { label: "Admin Panel", link: "/admin" },
-  ];
-
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Container>
@@ -32,52 +23,80 @@ const Navbar = () => {
             <Link href="/">
               <SiInternetcomputer />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.link}>
-                  <Link
-                    className={classNames({
-                      "text-zinc-900": link.link === currentPath,
-                      "text-zinc-500": link.link !== currentPath,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                    href={link.link}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="api/auth/signout">Sign Out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="api/auth/signin">Login</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", link: "/" },
+    { label: "Projects", link: "/projects/list" },
+    { label: "Admin Panel", link: "/admin" },
+  ];
+
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.link}>
+          <Link
+            className={classNames({
+              "nav-link": true,
+              "!text-zinc-900": link.link === currentPath,
+            })}
+            href={link.link}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return null;
+
+  if (status === "unauthenticated")
+    () => (
+      <Link className="nav-link" href="api/auth/signin">
+        Login
+      </Link>
+    );
+
+  return (
+    <Box>
+      {status === "authenticated" && (
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Avatar
+              src={session.user!.image!}
+              fallback="?"
+              size="2"
+              radius="full"
+              className="cursor-pointer"
+              referrerPolicy="no-referrer"
+            />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text size="2">{session.user!.email}</Text>
+            </DropdownMenu.Label>
+            <DropdownMenu.Item>
+              <Link href="api/auth/signout">Sign Out</Link>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      )}
+    </Box>
   );
 };
 
