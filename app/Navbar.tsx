5141290667d7b3a714ed8e9106b1302ad85204c9
Krish90s <krishnanvenkat4@gmail.com>
2024-01-06 10:25:07 +0530

    add navbar loading

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import Skeleton from "@/app/components/Skeleton";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { SiInternetcomputer } from "react-icons/si";
@@ -63,7 +64,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="3rem" />;
 
   if (status === "unauthenticated") {
     return (
